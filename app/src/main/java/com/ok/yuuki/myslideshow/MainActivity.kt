a8c8aa6023ba965d3e67a9474f66ba1a3b927f17
岡崎裕樹 <okazakiyuuki@okazaki.local>
2018-05-11 00:02:15 +0900

    画像表示処理実装

--- app/src/main/java/com/ok/yuuki/myslideshow/MainActivity.kt
+++ app/src/main/java/com/ok/yuuki/myslideshow/MainActivity.kt
@@ -2,11 +2,47 @@ package com.ok.yuuki.myslideshow
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.BounceInterpolator
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val resources = listOf(
+            R.drawable.slide00,
+            R.drawable.slide01,
+            R.drawable.slide02,
+            R.drawable.slide03,
+            R.drawable.slide04,
+            R.drawable.slide05,
+            R.drawable.slide06,
+            R.drawable.slide07,
+            R.drawable.slide08,
+            R.drawable.slide09
+    )
+
+    private var position = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageSwitcher.setFactory {
+            ImageView(this)
+        }
+        imageSwitcher.setImageResource(resources[0])
+
+        prevButton.setOnClickListener { movePosition(-1) }
+        nextButton.setOnClickListener { movePosition(1) }
+    }
+
+    private fun movePosition(move: Int) {
+        position += move
+        if (position >= resources.size) {
+            position = 0
+        } else if (position < 0) {
+            position = resources.size - 1
+        }
+        imageSwitcher.setImageResource(resources[position])
     }
 }
